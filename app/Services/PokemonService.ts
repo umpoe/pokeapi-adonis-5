35d417989b896604949b903efccc6a36e4dd2dc1
Umpo Ncube <umpo.ncube@oleconnect.com>
2024-01-10 21:10:56 +0200

    added pokemon details to the get cards endpoint

--- app/Services/PokemonService.ts
+++ app/Services/PokemonService.ts
@@ -14,6 +14,14 @@ class PokemonService {
       const {meta, data} = pokemons_
      let { perPage, currentPage, firstPage, isEmpty, total, hasTotal, lastPage, hasMorePages, hasPages } = meta
 
+     for (const key in data) {
+      if (Object.prototype.hasOwnProperty.call(data, key)) {
+        const element = data[key]
+        let pokemon_detail = await PokemonDetail.findBy('pokemon_id', element.id)
+        data[key].pokemon_detail = pokemon_detail
+      }
+     }
+
     return {
       data,
       totalItems: total,
